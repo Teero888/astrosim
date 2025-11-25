885ae770d396b745c66b9132fa8aea9a02415ab2
Teero888 <teero484@gmail.com>
2025-11-25 12:13:54 +0100

    allow multiple atmospheres

--- src/gfx/atmosphere.cpp
+++ src/gfx/atmosphere.cpp
@@ -3,8 +3,10 @@
 #include "camera.h"
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/ext/vector_float3.hpp"
+#include <algorithm>
 #include <cmath>
 #include <embedded_shaders.h>
+#include <vector>
 
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtc/quaternion.hpp>
@@ -32,11 +34,6 @@ void CAtmosphere::Init()
 
 void CAtmosphere::Render(CStarSystem &System, CCamera &Camera, unsigned int DepthTextureUnit, unsigned int ShadowTextureID, const glm::mat4 &LightSpaceMatrix, int DebugMode)
 {
-	SBody *pFocusedBody = Camera.m_pFocusedBody;
-
-	if(!pFocusedBody || !pFocusedBody->m_RenderParams.m_Atmosphere.m_Enabled)
-		return;
-
 	m_Shader.Use();
 
 	m_Shader.SetInt("u_depthTexture", DepthTextureUnit);
@@ -52,53 +49,6 @@ void CAtmosphere::Render(CStarSystem &System, CCamera &Camera, unsigned int Dept
 	float F = 2.0f / log2(FAR_PLANE + 1.0f);
 	m_Shader.SetFloat("u_logDepthF", F);
 
-	// == Rotation Handling ==
-	Quat q = pFocusedBody->m_SimParams.m_Orientation;
-
-	// 1. Pass Rotation Matrix for Shadow Map Lookup (Local -> World)
-	glm::quat glmQ(q.w, q.x, q.y, q.z);
-	glm::mat4 matRot = glm::mat4_cast(glmQ);
-	m_Shader.SetMat4("u_planetRotation", matRot);
-
-	// 2. Pass Inverse for Ray Direction (World -> Local)
-	Quat qInv = q.Conjugate();
-	glm::quat glmQInv(qInv.w, qInv.x, qInv.y, qInv.z);
-	glm::mat4 matRotInv = glm::mat4_cast(glmQInv);
-
-	glm::mat4 gridView = glm::lookAt(glm::vec3(0.0f), (glm::vec3)Camera.m_Front, (glm::vec3)Camera.m_Up);
-	glm::mat4 viewToWorld = glm::inverse(gridView);
-	glm::mat4 viewToLocal = matRotInv * viewToWorld;
-
-	m_Shader.SetMat4("u_invView", viewToLocal);
-	m_Shader.SetMat4("u_invProjection", glm::inverse(Camera.m_Projection));
-
-	double realRadius = pFocusedBody->m_RenderParams.m_Radius;
-	Vec3 relativeCamPos = Camera.m_AbsolutePosition - pFocusedBody->m_SimParams.m_Position;
-	Vec3 localCamPos = qInv.RotateVector(relativeCamPos);
-	Vec3 normalizedCamPos = localCamPos / realRadius;
-
-	m_Shader.SetVec3("u_cameraPos", (glm::vec3)normalizedCamPos);
-
-	Vec3 sunVector = System.m_pSunBody->m_SimParams.m_Position - pFocusedBody->m_SimParams.m_Position;
-	Vec3 sunDir;
-	if(sunVector.length() > 1.0)
-		sunDir = qInv.RotateVector(sunVector.normalize());
-	else
-		sunDir = Vec3(0, 1, 0);
-	m_Shader.SetVec3("u_sunDirection", (glm::vec3)sunDir);
-
-	m_Shader.SetFloat("u_realPlanetRadius", (float)realRadius);
-	m_Shader.SetFloat("u_sunIntensity", pFocusedBody->m_RenderParams.m_Atmosphere.m_SunIntensity);
-
-	float atmRadMeters = pFocusedBody->m_RenderParams.m_Atmosphere.m_AtmosphereRadius * (float)realRadius;
-	m_Shader.SetFloat("u_atmosphereRadius", atmRadMeters);
-
-	m_Shader.SetVec3("u_rayleighScatteringCoeff", pFocusedBody->m_RenderParams.m_Atmosphere.m_RayleighScatteringCoeff);
-	m_Shader.SetFloat("u_rayleighScaleHeight", pFocusedBody->m_RenderParams.m_Atmosphere.m_RayleighScaleHeight);
-	m_Shader.SetVec3("u_mieScatteringCoeff", pFocusedBody->m_RenderParams.m_Atmosphere.m_MieScatteringCoeff);
-	m_Shader.SetFloat("u_mieScaleHeight", pFocusedBody->m_RenderParams.m_Atmosphere.m_MieScaleHeight);
-	m_Shader.SetFloat("u_miePreferredScatteringDir", pFocusedBody->m_RenderParams.m_Atmosphere.m_MiePreferredScatteringDir);
-
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -106,7 +56,74 @@ void CAtmosphere::Render(CStarSystem &System, CCamera &Camera, unsigned int Dept
 	glDepthMask(GL_FALSE);
 
 	glBindVertexArray(m_Shader.VAO);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	// Collect bodies with atmosphere enabled
+	std::vector<SBody *> vBodiesToRender;
+	for(auto &Body : System.m_vBodies)
+	{
+		if(Body.m_RenderParams.m_Atmosphere.m_Enabled)
+			vBodiesToRender.push_back(&Body);
+	}
+
+	// Sort by distance to camera (Far to Near) for correct transparency blending
+	std::sort(vBodiesToRender.begin(), vBodiesToRender.end(), [&](SBody *a, SBody *b) {
+		double distA = (a->m_SimParams.m_Position - Camera.m_AbsolutePosition).length();
+		double distB = (b->m_SimParams.m_Position - Camera.m_AbsolutePosition).length();
+		return distA > distB;
+	});
+
+	glm::mat4 gridView = glm::lookAt(glm::vec3(0.0f), (glm::vec3)Camera.m_Front, (glm::vec3)Camera.m_Up);
+	glm::mat4 viewToWorld = glm::inverse(gridView);
+
+	for(SBody *pBody : vBodiesToRender)
+	{
+		Quat q = pBody->m_SimParams.m_Orientation;
+
+		// Pass Rotation Matrix for Shadow Map Lookup (Local -> World)
+		glm::quat glmQ(q.w, q.x, q.y, q.z);
+		glm::mat4 MatRot = glm::mat4_cast(glmQ);
+		m_Shader.SetMat4("u_planetRotation", MatRot);
+
+		// Pass Inverse for Ray Direction (World -> Local)
+		Quat qInv = q.Conjugate();
+		glm::quat glmQInv(qInv.w, qInv.x, qInv.y, qInv.z);
+		glm::mat4 MatRotInv = glm::mat4_cast(glmQInv);
+
+		glm::mat4 ViewToLocal = MatRotInv * viewToWorld;
+
+		m_Shader.SetMat4("u_invView", ViewToLocal);
+		m_Shader.SetMat4("u_invProjection", glm::inverse(Camera.m_Projection));
+
+		double RealRadius = pBody->m_RenderParams.m_Radius;
+		Vec3 RelativeCamPos = Camera.m_AbsolutePosition - pBody->m_SimParams.m_Position;
+		Vec3 LocalCamPos = qInv.RotateVector(RelativeCamPos);
+		Vec3 NormalizedCamPos = LocalCamPos / RealRadius;
+
+		m_Shader.SetVec3("u_cameraPos", (glm::vec3)NormalizedCamPos);
+
+		Vec3 SunVector = System.m_pSunBody->m_SimParams.m_Position - pBody->m_SimParams.m_Position;
+		Vec3 SunDir;
+		if(SunVector.length() > 1.0)
+			SunDir = qInv.RotateVector(SunVector.normalize());
+		else
+			SunDir = Vec3(0, 1, 0);
+		m_Shader.SetVec3("u_sunDirection", (glm::vec3)SunDir);
+
+		m_Shader.SetFloat("u_realPlanetRadius", (float)RealRadius);
+		m_Shader.SetFloat("u_sunIntensity", pBody->m_RenderParams.m_Atmosphere.m_SunIntensity);
+
+		float atmRadMeters = pBody->m_RenderParams.m_Atmosphere.m_AtmosphereRadius * (float)RealRadius;
+		m_Shader.SetFloat("u_atmosphereRadius", atmRadMeters);
+
+		m_Shader.SetVec3("u_rayleighScatteringCoeff", pBody->m_RenderParams.m_Atmosphere.m_RayleighScatteringCoeff);
+		m_Shader.SetFloat("u_rayleighScaleHeight", pBody->m_RenderParams.m_Atmosphere.m_RayleighScaleHeight);
+		m_Shader.SetVec3("u_mieScatteringCoeff", pBody->m_RenderParams.m_Atmosphere.m_MieScatteringCoeff);
+		m_Shader.SetFloat("u_mieScaleHeight", pBody->m_RenderParams.m_Atmosphere.m_MieScaleHeight);
+		m_Shader.SetFloat("u_miePreferredScatteringDir", pBody->m_RenderParams.m_Atmosphere.m_MiePreferredScatteringDir);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	}
+
 	glBindVertexArray(0);
 
 	glDepthMask(GL_TRUE);
